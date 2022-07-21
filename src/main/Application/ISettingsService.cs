9c089dcd51ca7af04efd3e04ff16d1a8318e705d
Junvic <contact@junvic.me>
2022-07-22 03:57:47 +0800

    refactor: move settings classes to correct layer

--- src/main/Application/ISettingsService.cs
+++ src/main/Application/ISettingsService.cs
@@ -1,6 +1,6 @@
 ﻿using ei8.Net.Http.Notifications;
 
-namespace ei8.Cortex.Subscriptions.Domain.Model
+namespace ei8.Cortex.Subscriptions.Application
 {
     public interface ISettingsService
     {
