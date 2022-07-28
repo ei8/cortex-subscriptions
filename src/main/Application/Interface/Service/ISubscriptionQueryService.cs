92bfed8746e14bca465c4ee6d4e87a51dcd20670
Junvic <contact@junvic.me>
2022-07-28 12:00:17 +0800

    refactor: move settings retrieval to query service

---
+++ src/main/Application/Interface/Service/ISubscriptionQueryService.cs
@@ -0,0 +1,10 @@
+﻿using ei8.Cortex.Subscriptions.Domain.Model;
+using System.Threading.Tasks;
+
+namespace ei8.Cortex.Subscriptions.Application.Interface.Service
+{
+    public interface ISubscriptionQueryService
+    {
+        Task<ServerConfiguration> GetServerConfigurationAsync();
+    }
+}
--- src/main/Port.Adapter/Out/Api/Program.cs
+++ src/main/Port.Adapter/Out/Api/Program.cs
@@ -1,10 +1,11 @@
 using ei8.Cortex.Subscriptions.Application;
-using ei8.Cortex.Subscriptions.Common;
+using ei8.Cortex.Subscriptions.Application.Interface.Service;
 using ei8.Cortex.Subscriptions.Port.Adapter.IO.Process.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<ISettingsService, EnvironmentSettingsService>();
+builder.Services.AddTransient<ISubscriptionQueryService, SubscriptionQueryService>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,12 +24,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/config", (ISettingsService settings) =>
+app.MapGet("/config", async (ISubscriptionQueryService subscriptionQueryService) =>
 {
-    return new SubscriptionConfiguration
-    {
-        ServerPublicKey = settings.PushSettings.PushPublicKey
-    };
+    return await subscriptionQueryService.GetServerConfigurationAsync();
 });
 
 app.Run();
